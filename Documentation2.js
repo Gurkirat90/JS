3e829f1e628c40b72bb49e09251229efd5d98ce9
GURKIRAT <gs544448@gmail.com>
2026-01-19 00:01:39 +0530

    Fix typo and add documentation comment

--- Documentation2.js
+++ Documentation2.js
@@ -2,3 +2,4 @@
 //"DOCUMENTATION IN JAVASCRIPT"
 console.log("Documentation in JavaScript");
 //This line of code prints "Documentation in JavaScript" to the console.
+//thid repo is just for my own learning
