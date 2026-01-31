25c0b20188d64929afb31dc9688a9c792a9c442e
GURKIRAT <gs544448@gmail.com>
2026-01-31 23:48:54 +0530

    Update Documentation2.js for clarity and learning

--- Documentation2.js
+++ Documentation2.js
@@ -3,3 +3,22 @@
 console.log("Documentation in JavaScript");
 //This line of code prints "Documentation in JavaScript" to the console.
 //thid repo is just for my own learning
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
